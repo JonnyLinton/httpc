e710d9f7a22576fc3fa2d5ae1ad8d7465094e014
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-26 14:27:25 -0500

    First Commit

---
+++ lab_assignment1.py
@@ -0,0 +1,13 @@
+import socket
+
+def get(url, port=80):
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connection.connect((url, port))
+        request = url.encode("utf-8")
+        connection.sendall(request)
+        response = connection.recv(len(request), socket.MSG_WAITALL)
+        print(response)
+    finally:
+        connection.close()
+get('http://httpbin.org/get?course=networking&assignment=1')
