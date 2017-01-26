c58fff62fcd07b7dedd51416845b65ed9a3aa33f
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-26 15:17:22 -0500

    Get hostname from URL

--- lab_assignment1.py
+++ lab_assignment1.py
@@ -1,12 +1,16 @@
 import socket
+from urllib.parse import urlparse
 
-def get(url, port=80):
+def get(url, port):
+    host = urlparse(url).hostname
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        connection.connect((url, port))
+        connection.connect((host, port))
         request = url.encode("utf-8")
         connection.sendall(request)
         response = connection.recv(len(request), socket.MSG_WAITALL)
-        print(response)
+        print(response.decode("utf-8"))
     finally:
         connection.close()
+
+get("http://httpbin.org/get?course=networking&assignment=1", 8007)
