68c08ffb03a42e14565508b8987afef05ee8aa10
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-26 17:51:32 -0500

    Added Query parse

--- lab_assignment1.py
+++ lab_assignment1.py
@@ -3,12 +3,12 @@ from urllib.parse import urlparse
 
 def get(url, port):
     host = urlparse(url).hostname
+    query = urlparse(url).query.encode("utf-8")
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         connection.connect((host, port))
-        request = url.encode("utf-8")
-        connection.sendall(request)
-        response = connection.recv(len(request), socket.MSG_WAITALL)
+        connection.sendall(query)
+        response = connection.recv(len(query), socket.MSG_WAITALL)
         print(response.decode("utf-8"))
     finally:
         connection.close()
