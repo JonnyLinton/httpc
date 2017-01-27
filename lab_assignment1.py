793de03f03ebc9617e2fe00a06afc774d08852ff
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-26 20:02:49 -0500

    Fixed GET formatting

--- lab_assignment1.py
+++ lab_assignment1.py
@@ -7,15 +7,15 @@ def get(url, port=80):
     host = urlparse(url).hostname
     # Retrieve query from URL
     query = urlparse(url).query
-    good_query = "GET /?%s HTTP/1.1\r\nHost: %s\n\r\n\r\n" % (query, host)
+    good_query = "GET %s HTTP/1.1\r\nHost: %s\n\r\n\r\n" % (url, host)
     print(good_query)
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         connection.connect((host, port))
         connection.sendall(good_query.encode("utf-8"))
-        response = connection.recv(8192, socket.MSG_WAITALL)
+        response = connection.recv(500, socket.MSG_WAITALL)
         sys.stdout.write(response.decode("utf-8"))
     finally:
         connection.close()
 
-get("http://httpbin.org/get?course=networking&assignment=1")
+get("http://www.google.com/?hello+world")
