83668026b0327a906d6b1baf523129980a35160a
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-26 19:53:24 -0500

    Added generic GET request

--- lab_assignment1.py
+++ lab_assignment1.py
@@ -1,16 +1,21 @@
 import socket
+import sys
 from urllib.parse import urlparse
 
-def get(url, port):
+def get(url, port=80):
+    # Retrieve hostname from passed URL
     host = urlparse(url).hostname
-    query = urlparse(url).query.encode("utf-8")
+    # Retrieve query from URL
+    query = urlparse(url).query
+    good_query = "GET /?%s HTTP/1.1\r\nHost: %s\n\r\n\r\n" % (query, host)
+    print(good_query)
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         connection.connect((host, port))
-        connection.sendall(query)
-        response = connection.recv(len(query), socket.MSG_WAITALL)
-        print(response.decode("utf-8"))
+        connection.sendall(good_query.encode("utf-8"))
+        response = connection.recv(8192, socket.MSG_WAITALL)
+        sys.stdout.write(response.decode("utf-8"))
     finally:
         connection.close()
 
-get("http://httpbin.org/get?course=networking&assignment=1", 80)
+get("http://httpbin.org/get?course=networking&assignment=1")
