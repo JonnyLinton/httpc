87eb2fefaf761437d25714b7a5c9265244e6bb66
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-29 17:01:20 -0500

    Removed redundant code

--- lab_assignment1.py
+++ lab_assignment1.py
@@ -6,13 +6,14 @@ from urllib.parse import urlparse
 def http_get(url, port=80):
     # Retrieve hostname from passed URL
     host = urlparse(url).hostname
-    # Retrieve query from URL
-    query_parameters = urlparse(url).query
-    full_get_query = "GET %s HTTP/1.1\r\nHost: %s\n\r\n\r\n" % (url, host)
+
+    # Format get query using URL and Host
+    query = "GET %s HTTP/1.1\r\nHost: %s\n\r\n\r\n" % (url, host)
+
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         connection.connect((host, port))
-        connection.sendall(full_get_query.encode("utf-8"))
+        connection.sendall(query.encode("utf-8"))
         response = connection.recv(4096)
         sys.stdout.write(response.decode("utf-8"))
     finally:
