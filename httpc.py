7dc82752a1722b5c85e0ba0b8d9f7db85ac3b1f2
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-01-30 12:59:33 -0500

    Added http_post support

--- httpc.py
+++ httpc.py
@@ -10,13 +10,12 @@ Options:
 
 """
 from docopt import docopt
-from lab_assignment1 import http_get
+from lab_assignment1 import http_get, http_post
 
 def run():
     args = docopt(__doc__)  # parse arguments based on docstring above
-
     # determine the http request type, and call the appropriate function
     if(args.get("get")):
         http_get(args.get("<url>"))
-    # if(args.get("post")):
-    #     http_post()
+    if(args.get("post")):
+        http_post(args.get("--headers"), args.get("--data"), args.get("<url>"))
--- lab_assignment1.py
+++ lab_assignment1.py
@@ -18,3 +18,20 @@ def http_get(url, port=80):
         sys.stdout.write(response.decode("utf-8"))
     finally:
         connection.close()
+
+def http_post(headers, data, url, port=80):
+
+    # Retrieve hostname from passed URL
+    host = urlparse(url).hostname
+
+    query = "POST /post HTTP/1.1\r\nHost: %s\n\r%s \n\r\n\r%s\n\r\n\r\n" % (host, headers, data)
+    sys.stdout.write(query)
+
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connection.connect((host, port))
+        connection.sendall(query.encode("utf-8"))
+        response = connection.recv(4096)
+        sys.stdout.write(response.decode("utf-8"))
+    finally:
+        connection.close()
