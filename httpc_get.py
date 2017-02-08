0198bee1f2689e1e57eae2adc4e61bb13a159d09
Louis-Olivier Guérin <guerin.louis@gmail.com>
2017-02-07 22:40:23 -0500

    Help now in respective http_X.py file

--- httpc_get.py
+++ httpc_get.py
@@ -1,5 +1,16 @@
+"""
+Usage: httpc get [-v] [-h key:value] URL
+Get executes a HTTP GET request for a given URL.
+   -v             Prints the detail of the response such as protocol, status, and headers.
+   -h key:value   Associates headers to HTTP Request with the format 'key:value'.
+"""
+
 from lab_assignment1 import sendRequest, getResponse, receiveResponse, printResponse
 from urllib.parse import urlparse
+from docopt import docopt
+
+if __name__ == '__main__':
+    print(docopt(__doc__))
 
 def http_get(url, verbose, port=80):
     # Retrieve hostname from passed URL
--- httpc_post.py
+++ httpc_post.py
@@ -1,5 +1,19 @@
+"""
+Usage: httpc post -v -h key:value -d inline-data -f file URL
+Post executes a HTTP POST request for a given URL with inline data or from file.
+   -v               Prints the detail of the response such as protocol, status, and headers.
+   -h key:value     Associates headers to HTTP Request with the format 'key:value'.
+   -d string        Associates an inline data to the body HTTP POST request.
+   -f file          Associates the content of a file to the body HTTP POST request.
+Either -d or -f can be used but not both.
+"""
+
 from lab_assignment1 import sendRequest, getResponse, receiveResponse, printResponse
 from urllib.parse import urlparse
+from docopt import docopt
+
+if __name__ == '__main__':
+    print(docopt(__doc__))
 
 def http_post(headers, data, url, verbose, port=80):
     # Retrieve hostname from passed URL
